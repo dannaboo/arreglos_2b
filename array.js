edb2f30dbe43480d023a8734ac021a3361706954
dannaboo <dannaboo444@gmail.com>
2025-03-18 09:16:52 -0600

    Add files via upload

---
+++ array.js
@@ -0,0 +1,41 @@
+/*
+let num = [10, 20, 30, 40, 50]
+console.log(num)
+
+let planetas = ["Marte", "Jupiter", "Saturno"]
+console.log(planetas)
+
+console.log("Primer planeta:", planetas[1]);
+console.log("Ultimo planeta:", planetas[planetas.legth -1])
+*/
+/*
+let planetas = ["venus", "Tierra","Marte", "Jupiter"]
+planetas.push("Saturno")
+console.log("Despues de push:", planetas)
+
+planetas.pop()
+console.log("Despues de pop:", planetas)
+planetas.unshift("Mercurio")
+console.log("Despues de unshift:", planetas.legth)
+
+planetas.shift()
+console.log("Despues de shift:", planetas)
+console.log("Numero de planetas:", planetas.length)
+
+console.log("Numeros de planetas:", planetas.length)
+console.log("El arreglo incluye a 'Venus'", planetas.includes("Venus"))*/
+//Ordenar arreglos
+/*
+let numeros = [3, 4, 5, 7, 2, 6]
+numeros.sort(); //ordena de menor a mayor
+console.log("Ordenados:", numeros)
+numeros.reverse();
+console.log("Invertidos:", numeros)
+*/
+//Arreglos multidimensionales 
+let matriz = [
+    ["Juan pablo", 18],
+    ["Alan", 19],
+    ["Valeria", 20]
+    ]
+    console.log("Edad de pablo:" ,matriz[0][1])
