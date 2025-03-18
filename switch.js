04035c9e48f565edc605f35c2256fd6016a84dfe
dannaboo <dannaboo444@gmail.com>
2025-03-17 21:35:02 -0600

    Se agrega el proyecto

---
+++ switch.js
@@ -0,0 +1,21 @@
+let fruit = 'Apple'
+
+switch(fruit){
+case 'StronBerry':
+    //code continue
+    console.log('is a StrownBerry')
+    break
+    case 'Watermelon':
+        //code continue
+        console.log('is a Watermelon')
+        break
+        case 'Apple':
+            //code continue
+            console.log('is a Apple')
+            break
+            default:
+                //code continue
+                console.log('Fruit not a exist')
+                break
+}
+
